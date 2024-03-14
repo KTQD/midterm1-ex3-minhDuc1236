aa7995bd3425040d606410021b31b1d13ad3ce8f
minhDuc1236 <163362874+minhDuc1236@users.noreply.github.com>
2024-03-14 08:10:54 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+
+            String inputPath = "input.txt";
+            String outputPath = "output.txt";
 
 
-        Bài làm viết tại đây
+            BufferedReader reader = new BufferedReader(new FileReader(inputPath));
+            StringBuilder contentBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
 
+                line = line.replaceAll("Nha Trang", "Vũng Tàu");
+                contentBuilder.append(line).append("\n");
+            }
+            reader.close();
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath));
+            writer.write(contentBuilder.toString());
+            writer.close();
+
+            System.out.println("Thay thế từ thành công và ghi vào file output.txt.");
+        } catch (IOException e) {
+            System.err.println("Xảy ra lỗi khi đọc hoặc ghi file: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
